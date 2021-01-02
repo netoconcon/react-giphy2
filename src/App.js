b0573b2f644232755609038aac2f9c42ffac48db
Neto Concon <concon.neto2@gmail.com>
2021-01-02 14:45:30 -0300

    add gif list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from "./components/search_bar"
-import Gif from "./components/gif"
+import SearchBar from "./components/search_bar";
+import Gif from "./components/gif";
+import GifList from './components/git_list';
 
 class App extends Component {
 	render(){
+
+    const gifs = [
+      { id: "l0amJzVHIAfl7jMDos" },
+      { id: "l0amJzVHIAfl7jMDos" }
+    ];
+  
+
     return (
       <div>
         <div className="left-scene">
@@ -14,6 +22,7 @@ class App extends Component {
           </div>
         </div>
         <div className="right-scene">
+					<GifList gifs={gifs} />
         </div>
       </div>
     );
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
-    // const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
     const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
     return(
       <img src={src} className="gif"/>
